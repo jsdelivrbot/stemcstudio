13c3b8da47f325106c24bfe02ea244cdff916b98
David Holmes <david.geo.holmes@gmail.com>
2015-04-22 20:55:36 -0400

    toggle mode

--- app/js/controllers/HomeController.ts
+++ app/js/controllers/HomeController.ts
@@ -1,6 +1,17 @@
 /// <reference path="../../../typings/angularjs/angular.d.ts" />
 var app = angular.module('app');
 
+var TEXT_CODE_HIDE = "Hide Code";
+var TEXT_CODE_SHOW = "Show Code";
+
 app.controller('HomeController', ['$scope', function($scope) {
-    $scope.title = "Home";
+
+  $scope.isCodeVisible = true;
+  $scope.toggleText = TEXT_CODE_HIDE;
+
+  $scope.toggleMode = function() {
+    $scope.isCodeVisible = !$scope.isCodeVisible;
+    $scope.toggleText = $scope.isCodeVisible ? TEXT_CODE_HIDE : TEXT_CODE_SHOW;
+  };
+
 }]);
