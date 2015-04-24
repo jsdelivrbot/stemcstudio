1207bc465aa886356f393a46aac093b6499ec06b
David Holmes <david.geo.holmes@gmail.com>
2015-04-24 16:46:28 -0400

    dist files

--- config/application.js
+++ config/application.js
@@ -34,6 +34,25 @@ module.exports = function(lineman) {
           {src: "node_modules/typescript/bin/lib.d.ts",                      dest: "generated/ts/lib.d.ts"},
           {src: "typings/threejs/three.d.ts",                                dest: "generated/ts/three.d.ts"}
         ]
+      },
+      dist: {
+        files: [
+          {src: "manual/ace-builds/src-min-noconflict/worker-workspace.js",  dest: "dist/js/worker-workspace.js"},
+          {src: "manual/ace-builds/src-min-noconflict/worker-javascript.js", dest: "dist/js/worker-javascript.js"},
+          {src: "manual/ace-builds/src-min-noconflict/worker-typescript.js", dest: "dist/js/worker-typescript.js"},
+      
+          {src: "bower_components/davinci-blade/dist/davinci-blade.min.js",    dest: "dist/js/blade.min.js"},
+          {src: "bower_components/davinci-eight/dist/davinci-eight.min.js",    dest: "dist/js/eight.min.js"},
+          {src: "bower_components/davinci-visual/dist/davinci-visual.min.js",  dest: "dist/js/visual.min.js"},
+          {src: "bower_components/davinci-mathscript/dist/davinci-mathscript.min.js",  dest: "dist/js/maths.min.js"},
+          {src: "bower_components/threejs/build/three.min.js",               dest: "dist/js/three.min.js"},
+      
+          {src: "bower_components/davinci-blade/dist/davinci-blade.d.ts",    dest: "dist/ts/blade.d.ts"},
+          {src: "bower_components/davinci-eight/dist/davinci-eight.d.ts",    dest: "dist/ts/eight.d.ts"},
+          {src: "bower_components/davinci-visual/dist/davinci-visual.d.ts",    dest: "dist/ts/visual.d.ts"},
+          {src: "node_modules/typescript/bin/lib.d.ts",                      dest: "dist/ts/lib.d.ts"},
+          {src: "typings/threejs/three.d.ts",                                dest: "dist/ts/three.d.ts"}
+        ]
       }
     }
 
--- configure.coffee
+++ configure.coffee
@@ -1,11 +1,7 @@
 nconf = require("nconf")
 
 defaultConfig =
-  PORT: 8080,
-  GITHUB_HOST: "github.com",
-  GITHUB_PORT: 443,
-  GITHUB_PATH: "/login/oauth/access_token",
-  GITHUB_METHOD: "POST"
+  PORT: 8080
 
 env = process.env.NODE_ENV or "local"
 console.log "using NODE_ENV=#{env}"
