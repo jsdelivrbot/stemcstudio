2c0a569c4671b4babea8890a623f1eaf6e4b12a8
David Holmes <david.geo.holmes@gmail.com>
2016-02-11 14:54:16 -0500

    Euclidean3

--- app/ts/services/templates/BOOTSTRAP_LESS.ts
+++ app/ts/services/templates/BOOTSTRAP_LESS.ts
@@ -1,10 +1,9 @@
+const NEWLINE = '\n'
+
 export default function(options: { stats?: boolean } = {}): string {
     const lines: string[] = []
     lines.push("body { margin: 0; }")
     lines.push("canvas { width: 600px; height: 600px }")
-    if (options.stats) {
-        lines.push("#stats { position: absolute; top: 0; left: 0; }")
-    }
-    lines.push("")
-    return lines.join('\n');
+    lines.push("#stats { position: absolute; top: 0; left: 0; }")
+    return lines.join(NEWLINE).concat(NEWLINE)
 }
--- app/ts/services/templates/BOOTSTRAP_LIBS.ts
+++ app/ts/services/templates/BOOTSTRAP_LIBS.ts
@@ -1,28 +1,29 @@
 export default function(): string {
-    const lines = "" +
-        "const zero = EIGHT.G3.zero\n" +
-        "const one = EIGHT.G3.one\n" +
-        "const e1 = EIGHT.G3.e1\n" +
-        "const e2 = EIGHT.G3.e2\n" +
-        "const e3 = EIGHT.G3.e3\n" +
-        "\n" +
-        "/**\n" +
-        " * The pseudoscalar for the Euclidean 3D Geometric Space, I = e1 * e2 * e3.\n" +
-        " */\n" +
-        "const I  = e1 * e2 * e3\n" +
-        "\n" +
-        "const exp = EIGHT.exp\n" +
-        "const log = EIGHT.log\n" +
-        "const cos = EIGHT.cos\n" +
-        "const sin = EIGHT.sin\n" +
-        "\n" +
-        "/**\n" +
-        " * dual(m), sign depends on who you talk to.\n" +
-        " */\n" +
-        "function dual(m: EIGHT.G3): EIGHT.G3 {\n" +
-        "  // return m << (I / (I * I)) // Dorst, Fontijne, Mann\n" +
-        "  return I * m // Hestenes, Doran, Lasenby\n" +
-        "  // return m * (I / (I * I)) // Hestenes, Doran, Lasenby\n" +
-        "}\n"
-    return lines;
+    const lines: string[] = [
+        "const zero = EIGHT.Euclidean3.zero",
+        "const one = EIGHT.Euclidean3.one",
+        "const e1 = EIGHT.Euclidean3.e1",
+        "const e2 = EIGHT.Euclidean3.e2",
+        "const e3 = EIGHT.Euclidean3.e3",
+        "",
+        "/**",
+        " * The pseudoscalar for the Euclidean 3D Geometric Space.",
+        " */",
+        "const I  = e1 ^ e2 ^ e3",
+        "",
+        "const exp = EIGHT.exp",
+        "const log = EIGHT.log",
+        "const cos = EIGHT.cos",
+        "const sin = EIGHT.sin",
+        "",
+        "/**",
+        " * dual(m), sign depends on who you talk to.",
+        " */",
+        "function dual(m: EIGHT.Euclidean3): EIGHT.Euclidean3 {",
+        "  // return m << (I / (I * I)) // Dorst, Fontijne, Mann",
+        "  return I * m // Hestenes, Doran, Lasenby",
+        "}",
+        ""
+        ]
+    return lines.join('\n');
 }
