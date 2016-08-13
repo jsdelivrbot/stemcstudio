85de6d670bd9e279edb740f7c9b6e65478c56fd5
David Holmes <david.geo.holmes@gmail.com>
2016-08-12 22:28:34 -0400

    Track

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -134,7 +134,7 @@ function vendorPath(packageFolder: string, fileName: string): string {
 }
 
 // The application version.
-app.constant('version', '2.7.30');
+app.constant('version', '2.7.31');
 
 // Feature flags (boolean)
 app.constant('FEATURE_AWS_ENABLED', false);
