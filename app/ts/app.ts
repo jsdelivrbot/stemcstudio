5a8d4b483c4afcff2b0f7391d766d0ac19635a01
David Holmes <david.geo.holmes@gmail.com>
2016-09-27 23:41:43 -0400

    theme

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -138,7 +138,7 @@ function vendorPath(packageFolder: string, fileName: string): string {
 }
 
 // The application version.
-app.constant('version', '2.7.71');
+app.constant('version', '2.7.72');
 
 // Feature flags (boolean)
 app.constant('FEATURE_AWS_ENABLED', false);
---
+++ app/ts/modules/themes/services/manager/originals.ts
@@ -0,0 +1,213 @@
+import Theme from '../../Theme';
+
+/**
+ * This is the complete list of (some) really ugly themes!
+ */
+const themes: Theme[] = [
+    {
+        name: "Ambiance",
+        isDark: true,
+        cssClass: 'ace-ambiance',
+        fileName: 'ambiance.css'
+    },
+    {
+        name: "Chaos",
+        isDark: true,
+        cssClass: 'ace-chaos',
+        fileName: 'chaos.css'
+    },
+    {
+        name: "Chrome",
+        isDark: false,
+        cssClass: 'ace-chrome',
+        fileName: 'chrome.css'
+    },
+    {
+        name: "Clouds",
+        isDark: false,
+        cssClass: 'ace-clouds',
+        fileName: 'clouds.css'
+    },
+    {
+        name: "Clouds Midnight",
+        isDark: true,
+        cssClass: 'ace-clouds-midnight',
+        fileName: 'clouds_midnight.css'
+    },
+    {
+        name: "Cobalt",
+        isDark: true,
+        cssClass: 'ace-cobalt',
+        fileName: 'cobalt.css'
+    },
+    {
+        name: "Crimson",
+        isDark: false,
+        cssClass: 'ace-crimson-editor',
+        fileName: 'crimson_editor.css'
+    },
+    {
+        name: "Dawn",
+        isDark: false,
+        cssClass: 'ace-dawn',
+        fileName: 'dawn.css'
+    },
+    {
+        name: "Dreamweaver",
+        isDark: false,
+        cssClass: 'ace-dreamweaver',
+        fileName: 'dreamweaver.css'
+    },
+    {
+        name: "Eclipse",
+        isDark: false,
+        cssClass: 'ace-eclipse',
+        fileName: 'eclipse.css'
+    },
+    {
+        name: "GitHub",
+        isDark: false,
+        cssClass: 'ace-github',
+        fileName: 'github.css'
+    },
+    {
+        name: "Idle Fingers",
+        isDark: true,
+        cssClass: 'ace-idle-fingers',
+        fileName: 'idle_fingers.css'
+    },
+    {
+        name: "iPlastic",
+        isDark: false,
+        cssClass: 'ace-iplastic',
+        fileName: 'iplastic.css'
+    },
+    {
+        name: "Katzenmilch",
+        isDark: false,
+        cssClass: 'ace-katzenmilch',
+        fileName: 'katzenmilch.css'
+    },
+    {
+        name: "KR",
+        isDark: true,
+        cssClass: 'ace-kr-theme',
+        fileName: 'kr_theme.css'
+    },
+    {
+        name: "Kuroir",
+        isDark: false,
+        cssClass: 'ace-kuroir',
+        fileName: 'kuroir.css'
+    },
+    {
+        name: "Merbivore",
+        isDark: true,
+        cssClass: 'ace-merbivore',
+        fileName: 'merbivore.css'
+    },
+    {
+        name: "Merbivore Soft",
+        isDark: true,
+        cssClass: 'ace-merbivore-soft',
+        fileName: 'merbivore_soft.css'
+    },
+    {
+        name: "Mono Industrial",
+        isDark: true,
+        cssClass: 'ace-mono-industrial',
+        fileName: 'mono_industrial.css'
+    },
+    {
+        name: "Monokai",
+        isDark: true,
+        cssClass: 'ace-monokai',
+        fileName: 'monokai.css'
+    },
+    {
+        name: "Pastel On Dark",
+        isDark: true,
+        cssClass: 'ace-pastel-on-dark',
+        fileName: 'pastel_on_dark.css'
+    },
+    {
+        name: "Solarized Dark",
+        isDark: true,
+        cssClass: 'ace-solarized-dark',
+        fileName: 'solarized_dark.css'
+    },
+    {
+        name: "Solarized Light",
+        isDark: false,
+        cssClass: 'ace-solarized-light',
+        fileName: 'solarized_light.css'
+    },
+    {
+        name: "SQL Server",
+        isDark: false,
+        cssClass: 'ace-sqlserver',
+        fileName: 'sqlserver.css'
+    },
+    {
+        name: "Terminal",
+        isDark: true,
+        cssClass: 'ace-terminal-theme',
+        fileName: 'terminal.css'
+    },
+    {
+        name: "TextMate",
+        isDark: false,
+        cssClass: 'ace-tm',
+        fileName: 'textmate.css'
+    },
+    {
+        name: "Tomorrow",
+        isDark: false,
+        cssClass: 'ace-tomorrow',
+        fileName: 'tomorrow.css'
+    },
+    {
+        name: "Tomorrow Night",
+        isDark: true,
+        cssClass: 'ace-tomorrow-night',
+        fileName: 'tomorrow_night.css'
+    },
+    {
+        name: "Tomorrow Night Blue",
+        isDark: true,
+        cssClass: 'ace-tomorrow-night-blue',
+        fileName: 'tomorrow_night_blue.css'
+    },
+    {
+        name: "Tomorrow Night Bright",
+        isDark: true,
+        cssClass: 'ace-tomorrow-night-bright',
+        fileName: 'tomorrow_night_bright.css'
+    },
+    {
+        name: "Tomorrow Night Eighties",
+        isDark: true,
+        cssClass: 'ace-tomorrow-night-eighties',
+        fileName: 'tomorrow_night_eighties.css'
+    },
+    {
+        name: "Twilight",
+        isDark: true,
+        cssClass: 'ace-twilight',
+        fileName: 'twilight.css'
+    },
+    {
+        name: "Vibrant Ink",
+        isDark: true,
+        cssClass: 'ace-vibrant-ink',
+        fileName: 'vibrant_ink.css'
+    },
+    {
+        name: "Xcode",
+        isDark: false,
+        cssClass: 'ace-xcode',
+        fileName: 'xcode.css'
+    },
+];
+
+export default themes;
