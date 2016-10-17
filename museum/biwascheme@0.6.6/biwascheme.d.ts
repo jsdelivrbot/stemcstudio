546f5e7be85153fea0aca41b17fa9119356c95fb
David Holmes <david.geo.holmes@gmail.com>
2016-10-16 21:28:55 -0400

    BiwaScheme.d.ts

--- museum/biwascheme@0.6.6/biwascheme.d.ts
+++ museum/biwascheme@0.6.6/biwascheme.d.ts
@@ -9,15 +9,16 @@ declare module BiwaScheme {
     class Interpreter {
         constructor(errorHandler?: (e: { message: string }) => any);
         evaluate(code: string, after_evaluate?: (result: Object) => any): void;
-        static assert_integer(x: any): void;
-        static define_libfunc(name: string, minArgs: number, maxArgs: number, body: (ar: any[]) => any): void;
-        static isClosure(x: any): boolean;
-        static isList(x: any): boolean;
-        static isPair(x: any): x is Pair;
-        static to_write(obj: Object): string;
     }
 
     class Pair {
         constructor(car: any, cdr: any);
     }
+
+    function assert_integer(x: any): void;
+    function define_libfunc(name: string, minArgs: number, maxArgs: number, body: (ar: any[]) => any): void;
+    function isClosure(x: any): boolean;
+    function isList(x: any): boolean;
+    function isPair(x: any): x is Pair;
+    function to_write(obj: Object): string;
 }
