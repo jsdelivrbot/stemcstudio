e5c161e2a002b8005f4e7eabad5215a289ff9b15
David Holmes <david.geo.holmes@gmail.com>
2016-10-16 21:18:48 -0400

    BiwaScheme.d.ts

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -138,7 +138,7 @@ function vendorPath(packageFolder: string, fileName: string): string {
 }
 
 // The application version.
-app.constant('version', '2.7.85');
+app.constant('version', '2.7.86');
 
 // Feature flags (boolean)
 app.constant('FEATURE_AWS_ENABLED', false);
--- museum/biwascheme@0.6.6/biwascheme.d.ts
+++ museum/biwascheme@0.6.6/biwascheme.d.ts
@@ -7,7 +7,17 @@
 declare module BiwaScheme {
 
     class Interpreter {
-        constructor(errorHandler: (e: { message: string }) => any);
-        evaluate(code: string): void;
+        constructor(errorHandler?: (e: { message: string }) => any);
+        evaluate(code: string, after_evaluate?: (result: Object) => any): void;
+        static assert_integer(x: any): void;
+        static define_libfunc(name: string, minArgs: number, maxArgs: number, body: (ar: any[]) => any): void;
+        static isClosure(x: any): boolean;
+        static isList(x: any): boolean;
+        static isPair(x: any): x is Pair;
+        static to_write(obj: Object): string;
+    }
+
+    class Pair {
+        constructor(car: any, cdr: any);
     }
 }
