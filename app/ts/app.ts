6b063a7651261776c49ab9ca8729cd9ba658722b
David Holmes <david.geo.holmes@gmail.com>
2016-10-30 18:19:24 -0400

    textures

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -138,7 +138,7 @@ function vendorPath(packageFolder: string, fileName: string): string {
 }
 
 // The application version.
-app.constant('version', '2.7.107');
+app.constant('version', '2.7.108');
 
 // Feature flags (boolean)
 app.constant('FEATURE_AWS_ENABLED', false);
