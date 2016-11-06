b4d0f48fd0b6613d9866c56be31c7dd6b7c7ef9e
David Holmes <david.geo.holmes@gmail.com>
2016-11-05 22:13:55 -0400

    EIGHT

--- dist/jspm.config.js
+++ dist/jspm.config.js
@@ -108,7 +108,7 @@ System.config({
     },
     "npm:angular2@2.0.0-beta.17": {
       "reflect-metadata": "npm:reflect-metadata@0.1.2",
-      "rxjs": "npm:rxjs@5.0.0-rc.1",
+      "rxjs": "npm:rxjs@5.0.0-rc.2",
       "zone.js": "npm:zone.js@0.6.26"
     },
     "npm:asn1.js@4.8.1": {
@@ -508,7 +508,7 @@ System.config({
       "buffer": "github:jspm/nodelibs-buffer@0.1.0",
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
-    "npm:rxjs@5.0.0-rc.1": {
+    "npm:rxjs@5.0.0-rc.2": {
       "buffer": "github:jspm/nodelibs-buffer@0.1.0",
       "process": "github:jspm/nodelibs-process@0.1.2",
       "symbol-observable": "npm:symbol-observable@1.0.4"
