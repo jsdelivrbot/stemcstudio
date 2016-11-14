175365526ac39e813f96f049578af3d3607dd483
David Holmes <david.geo.holmes@gmail.com>
2016-11-13 21:11:32 -0500

    skins

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -138,7 +138,7 @@ function vendorPath(packageFolder: string, fileName: string): string {
 }
 
 // The application version.
-app.constant('version', '2.8.24');
+app.constant('version', '2.8.25');
 
 // Feature flags (boolean)
 app.constant('FEATURE_AWS_ENABLED', false);
