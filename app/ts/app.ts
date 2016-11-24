399396d074c23e418c59a6a5840bf3541c1e7ed9
David Holmes <david.geo.holmes@gmail.com>
2016-11-23 21:19:46 -0500

    minecraft

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -138,7 +138,7 @@ function vendorPath(packageFolder: string, fileName: string): string {
 }
 
 // The application version.
-app.constant('version', '2.8.39');
+app.constant('version', '2.8.40');
 
 // Feature flags (boolean)
 app.constant('FEATURE_AWS_ENABLED', false);
