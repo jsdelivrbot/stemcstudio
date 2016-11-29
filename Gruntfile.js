7993c19b0a8ff6809d45c0b4f39f7484ce62c08e
David Holmes <david.geo.holmes@gmail.com>
2016-11-29 18:17:32 -0500

    EIGHT

--- Gruntfile.js
+++ Gruntfile.js
@@ -488,15 +488,15 @@ function copies(target) {
         },
         {
             src: "../davinci-eight/dist/davinci-eight.d.ts",
-            dest: prepend(target, 'vendor/davinci-eight@4.0.6/dist/davinci-eight.d.ts')
+            dest: prepend(target, 'vendor/davinci-eight@4.0.7/dist/davinci-eight.d.ts')
         },
         {
             src: "../davinci-eight/dist/davinci-eight.js",
-            dest: prepend(target, 'vendor/davinci-eight@4.0.6/dist/davinci-eight.js')
+            dest: prepend(target, 'vendor/davinci-eight@4.0.7/dist/davinci-eight.js')
         },
         {
             src: "../davinci-eight/dist/davinci-eight.css",
-            dest: prepend(target, 'vendor/davinci-eight@4.0.6/dist/davinci-eight.css')
+            dest: prepend(target, 'vendor/davinci-eight@4.0.7/dist/davinci-eight.css')
         },
         {
             src: "bower_components/davinci-mathscript/dist/davinci-mathscript.d.ts",
--- app/ts/app.ts
+++ app/ts/app.ts
@@ -138,7 +138,7 @@ function vendorPath(packageFolder: string, fileName: string): string {
 }
 
 // The application version.
-app.constant('version', '2.10.4');
+app.constant('version', '2.10.5');
 
 // Feature flags (boolean)
 app.constant('FEATURE_AWS_ENABLED', false);
