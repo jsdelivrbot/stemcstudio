c8cc64db863a766f02b680e2a1aa67b95f407456
David Holmes <david.geo.holmes@gmail.com>
2017-01-30 22:57:40 -0500

    NEWTON

---
+++ dist/jspm_packages/npm/showdown@1.6.3/src/subParsers/encodeAmpsAndAngles.js
@@ -0,0 +1,11 @@
+/* */ 
+(function(process) {
+  showdown.subParser('encodeAmpsAndAngles', function(text, options, globals) {
+    'use strict';
+    text = globals.converter._dispatch('encodeAmpsAndAngles.before', text, options, globals);
+    text = text.replace(/&(?!#?[xX]?(?:[0-9a-fA-F]+|\w+);)/g, '&amp;');
+    text = text.replace(/<(?![a-z\/?$!])/gi, '&lt;');
+    text = globals.converter._dispatch('encodeAmpsAndAngles.after', text, options, globals);
+    return text;
+  });
+})(require('process'));
---
+++ jspm_packages/npm/showdown@1.6.3.js
@@ -0,0 +1,1 @@
+module.exports = require("npm:showdown@1.6.3/dist/showdown.js");
