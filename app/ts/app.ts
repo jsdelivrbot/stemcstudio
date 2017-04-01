464c30cfdcb677067151b73c7133840ddcd7910f
David Holmes <david.geo.holmes@gmail.com>
2017-03-31 22:29:54 -0400

    cleanup

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -152,7 +152,7 @@ function vendorPath(packageFolder: string, fileName: string): string {
 }
 
 // The application version.
-app.constant('version', '2.23.3');
+app.constant('version', '2.23.4');
 
 // Feature flags (boolean)
 app.constant('FEATURE_AWS_ENABLED', false);
