4a309acd0762d40ebd65e1d8088f2a44b9443d70
David Holmes <david.geo.holmes@gmail.com>
2017-07-28 15:55:19 -0400

    version

--- app/ts/constants.ts
+++ app/ts/constants.ts
@@ -3,7 +3,7 @@
  * This file is used to load the application and the workers.
  * DON'T FORGET TO CHANGE THE VERSION IN index.pug !!!
  */
-export const APP_VERSION = '2.36.13';
+export const APP_VERSION = '2.36.14';
 
 /**
  * Web Worker supporting TypeScript Mode.
