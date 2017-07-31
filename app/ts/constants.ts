88638f7981d26b9b288dd05d2f60c9f3ad8f10bc
David Holmes <david.geo.holmes@gmail.com>
2017-07-31 16:47:24 -0400

    sprites

--- app/ts/constants.ts
+++ app/ts/constants.ts
@@ -3,7 +3,7 @@
  * This file is used to load the application and the workers.
  * DON'T FORGET TO CHANGE THE VERSION IN index.pug !!!
  */
-export const APP_VERSION = '2.38.0';
+export const APP_VERSION = '2.38.1';
 
 /**
  * Web Worker supporting TypeScript Mode.
